a3004c2ae09c88afcaa6834bd91674af3062c3c7
971638267 <971638267@qq.com>
2017-03-31 10:03:41 +0800

    Update UrlConstant.java
    加入缓存启用开关

--- app/src/main/java/com/yunpai/tms/constant/UrlConstant.java
+++ app/src/main/java/com/yunpai/tms/constant/UrlConstant.java
@@ -1,8 +1,9 @@
 package com.yunpai.tms.constant;
 
 public class UrlConstant {
+    public static boolean CACHE=true;//是否启用缓存
     //自己的服务器
     //public static String BASEURL = "http://118.178.33.25:8080/CRUD/";
     public static String BASEURL = "https://api.douban.com/v2/movie/";
-
+    
 }
