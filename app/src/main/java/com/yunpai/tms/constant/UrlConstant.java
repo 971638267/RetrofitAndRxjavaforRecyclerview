80455e6aca8122a26ced1d631c5092a77766548d
ganyufei <971638267@qq.com>
2017-05-23 14:23:41 +0800

    1.删除多余代码

--- app/src/main/java/com/yunpai/tms/constant/UrlConstant.java
+++ app/src/main/java/com/yunpai/tms/constant/UrlConstant.java
@@ -2,8 +2,6 @@ package com.yunpai.tms.constant;
 
 public class UrlConstant {
     //自己的服务器
-    //public static String BASEURL = "http://118.178.33.25:8080/";
     public static String BASEURL = "https://api.douban.com/v2/movie/";
-    //    public static String BASEURL = "http://121.196.244.191:8090";
     public static boolean CACHE = true;
 }
